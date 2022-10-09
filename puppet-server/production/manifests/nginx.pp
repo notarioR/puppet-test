a3001222e773f54d3fb16dd6ce66e1d4e4df5fa0
notarioR <notariorojo@gmail.com>
2022-10-09 22:53:07 +0200

    Proxy to redirect request for domain.com and domain.com/resource2

--- puppet-server/production/manifests/nginx.pp
+++ puppet-server/production/manifests/nginx.pp
@@ -4,4 +4,24 @@ node puppetclient {
       package_source => 'nginx-stable',
   }
 
+  nginx::resource::server{'domain.com':
+    ensure                => present,
+    listen_port           => 443,
+    ssl                   => true,
+    ssl_port              => 443,
+    ssl_cert              => '/home/user/localhost.crt',
+    ssl_key               => '/home/user/localhost.key',
+    proxy                 => 'http://10.10.10.10',
+    access_log            => '/var/log/nginx/puppet_access.log',
+    error_log             => '/var/log/nginx/puppet_error.log'
+  }
+ 
+  nginx::resource::location{'/resource2':
+    ensure          => present,
+    ssl             => true,
+    ssl_only        => true,
+    proxy => 'http://20.20.20.20/' ,
+    server => 'domain.com'
+  }
+
 }
